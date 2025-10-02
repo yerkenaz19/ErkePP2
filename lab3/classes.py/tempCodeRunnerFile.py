7b378f0af36cc9ca50228d6bd9021051f6b66428
yerkenaz19 <y_sagynbayeva@kbtu.kz>
2025-10-02 17:27:42 +0500

    l3

--- lab3/classes.py/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-class Onstrings:
-    def getString(self):
-        self.s=input()
-    def printString(self):
-        print(self.s.upper())
-p1=Onstrings() #создается объект класса p1
-p1.getString()
-p1.printString() 
